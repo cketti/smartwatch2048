aa65eb35319057ac0c1e3061ca97fcfb9f22746d
cketti <cketti@gmail.com>
2014-05-12 23:24:10 +0200

    Allow playing the free version until a 1024 tile was created

--- app/src/main/java/sexy/fairly/smartwatch/game2048/Game.java
+++ app/src/main/java/sexy/fairly/smartwatch/game2048/Game.java
@@ -7,7 +7,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Game {
-    public static final int FREE_LIMIT = 512;
+    public static final int FREE_LIMIT = 1024;
 
     private final InsertCellCallback mCallback;
     private Grid mGrid;
